c14670bcd756886d3e640e0c2692f17b40a7e0a2
David Salazar <davids1_4@hotmail.com>
2021-04-22 13:41:26 +0200

    Inputs y formato

--- modulo02/program.py
+++ modulo02/program.py
@@ -47,3 +47,13 @@ print(cadena.replace("hola", "adiós"))
 print(cadena.lstrip())
 print(cadena.rstrip())
 print(cadena.split())
+
+# Inputs y formato
+nombre = input("Dime tu nombre: ")
+numero = 10/3
+print(nombre)
+print(f"Tu nombre es: {nombre}")
+print("Tu nombre es: {}".format(nombre))
+print("Tu nombre es: {n}".format(n = nombre))
+print("Tu nombre es:", nombre)
+print("Resultado de número: {n:1.2f}".format(n=numero))
