203a6e7961ecd190e7b76600c176c01fbbf5ebca
David Salazar <davids1_4@hotmail.com>
2021-04-22 12:58:08 +0200

    Casting de tipos

--- modulo02/program.py
+++ modulo02/program.py
@@ -1,3 +1,5 @@
+# Tipos de datos
+
 numero = 10
 Numero = 20
 saludo = "Hola a todos !!"
@@ -9,3 +11,22 @@ print("Saludo: " +saludo)
 
 print(type(numero))
 print(type(saludo))
+
+# Asignación simultánea
+a = 10
+b = 5
+
+a,b = b,a
+
+print(a)
+print(b)
+
+# Casting de tipos
+a = 5
+b = "25"
+c = "25.7"
+d = 25.8999
+print("Número: " + str(a))
+print(int(b))
+print(float(c))
+print(int(d))
