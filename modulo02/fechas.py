4017d2c05bb39406aa0e5c01d2c9296aa8d6ef41
David Salazar <davids1_4@hotmail.com>
2021-04-26 10:20:38 +0200

    Actualización fechas

--- modulo02/fechas.py
+++ modulo02/fechas.py
@@ -8,3 +8,14 @@ print('Año:', datenow2.year)
 print('Mes:', datenow2.month)
 print('Dia:', datenow2.day)
 print(f'Hora: {datenow2.hour}:{datenow2.minute}')
+
+# Parse fechas
+print("Escribe tu cumpleaños:")
+fecha = input() #d-m-y
+dt3 = datetime.strptime(fecha, '%d-%m-%Y').date() #string a date
+print('Fecha parse: ', dt3 )
+
+# Operaciones entre fechas
+fechaAct = datetime.now().date()
+fechaCump = fechaAct.year - dt3.year
+print(f'Tienes {fechaCump} años')
