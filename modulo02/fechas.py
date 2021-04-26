9d4c32b963919652b0690b1552cf7055820366c8
David Salazar <davids1_4@hotmail.com>
2021-04-26 10:36:34 +0200

    Fomato fechas

--- modulo02/fechas.py
+++ modulo02/fechas.py
@@ -11,7 +11,8 @@ print(f'Hora: {datenow2.hour}:{datenow2.minute}')
 
 # Parse fechas
 print("Escribe tu cumpleaños:")
-fecha = input() #d-m-y
+# fecha = input() #d-m-y
+fecha = '26-07-1992'
 dt3 = datetime.strptime(fecha, '%d-%m-%Y').date() #string a date
 print('Fecha parse: ', dt3 )
 
@@ -19,3 +20,8 @@ print('Fecha parse: ', dt3 )
 fechaAct = datetime.now().date()
 fechaCump = fechaAct.year - dt3.year
 print(f'Tienes {fechaCump} años')
+
+# Fomatear fechas
+fechaFormat = datetime.now()
+print(fechaFormat.strftime('%A, %d de %b de %Y '))
+print(fechaFormat.strftime('Día %j del año %Y'))
