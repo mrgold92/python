82b08265a9e4100285fc6ee60d650a7a931627ef
David Salazar <davids1_4@hotmail.com>
2021-04-28 21:40:40 +0200

    Cambiada visibilidad atributos clases.py

--- modulo04/clases.py
+++ modulo04/clases.py
@@ -20,54 +20,52 @@ from datetime import datetime
 
 
 class Alumno:
-    # __nombreatributo is private
-    # _nombreatrubuto is protected
-    # nombreatributo is public
-    __nombre = ""
-    __apellidos = ""
-    __fechaNacimiento = ""
-    __edad = 0
+    
+    nombre = ""
+    apellidos = ""
+    fechaNacimiento = ""
+    edad = 0
 
 
     def __init__(self, nombre, apellidos) -> None:
-        self.__nombre = nombre
-        self.__apellidos = apellidos
+        self.nombre = nombre
+        self.apellidos = apellidos
 
     def saluda(self) -> None:
-        print(f'Hola {self.__nombre} {self.__apellidos} !!!')
+        print(f'Hola {self.nombre} {self.apellidos} !!!')
 
     def setfechaNacimiento(self, fecha) -> None:
         try:
             if(len(fecha) == 8):
-                self.__fechaNacimiento = datetime.strptime(
+                self.fechaNacimiento = datetime.strptime(
                     fecha, '%d-%m-%y').date()
             else:
-                self.__fechaNacimiento = datetime.strptime(
+                self.fechaNacimiento = datetime.strptime(
                     fecha, '%d-%m-%Y').date()
             self.__calcularEdad()
         except:
             print("Formato incorrecto. [dd-mm-yyyy] || [dd-mm-yy] ")
 
     def __calcularEdad(self):
-        self.__edad = datetime.now().date().year - self.__fechaNacimiento.year
+        self.edad = datetime.now().date().year - self.fechaNacimiento.year
 
     def getFechaNacimiento(self) -> datetime:
-        return self.__fechaNacimiento
+        return self.fechaNacimiento
 
     def getEdad(self) -> int:
         try:
-            if(self.__edad == 0):
+            if(self.edad == 0):
                 raise Exception("Debes incluir la fecha de nacimiento.")
             else:
-                return self.__edad
+                return self.edad
         except Exception as e:
             print(e)
 
     def getNombre(self) -> str:
-        return self.__nombre
+        return self.nombre
 
     def getApellidos(self) -> str:
-        return self.__apellidos
+        return self.apellidos
 
 
 alumno = Alumno("Pepito", "Pérez")
