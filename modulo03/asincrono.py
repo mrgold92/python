07eca9fafcf05168ba4ccd7329d0181422ff3a39
David Salazar <davids1_4@hotmail.com>
2021-04-28 13:06:04 +0200

    Actualizado asíncrono

--- modulo03/asincrono.py
+++ modulo03/asincrono.py
@@ -5,7 +5,7 @@
 # Se utiliza como base para múltiples marcos
 # asíncronos de Python que proporcionan redes
 # y servidores web de alto rendimiento, bibliotecas
-# de conexión de bases de datos, colas de tareas,
+# de conexión de bases de datos, colas de tareas
 # distribuídas, etc.
 #
 # Permite realizar diferentes tareas al mismo tiempo.
@@ -17,5 +17,6 @@ async def main():
     print("Hello...")
     await asyncio.sleep(1)
     print("... world")
-
+    
+# Python 3.7+
 asyncio.run(main())
