972ab30101260a3bd27fa4cf89adcef0c9bc46d5
David Salazar <davids1_4@hotmail.com>
2021-04-30 09:45:48 +0200

    Modificado módulos

--- modulo04/modulos.py
+++ modulo04/modulos.py
@@ -1,5 +1,18 @@
+#----------------------------------------------#
+# Para importar de diferentes carpetas:
+#
+# import sys
+# sys.path.insert(0, 'ruta') 
+# #tantas líneas insert como
+# importanciones tengamos, subiendo el 0,1,2..
+#
+# from archivo import clase
+# o
+# from archivo import clase as alias
+#-----------------------------------------------#
 
 from clases import Alumno
+
 
 alumno = Alumno("Lucas", "Martín")
 alumno.saluda()
