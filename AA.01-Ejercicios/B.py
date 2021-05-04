4c4d68f50104eaf0f290931f23e38e9f3041cc5d
David Salazar <davids1_4@hotmail.com>
2021-05-04 11:42:26 +0200

    Actualizado ejercicios/B

--- AA.01-Ejercicios/B.py
+++ AA.01-Ejercicios/B.py
@@ -3,7 +3,9 @@
 # de multiplicarlo por PI. Utiliza la constante "math.pi"
 # y recuerda incluir "import math"
 #
-# B2. Muestra la raíz cuadrada del mismo número
+# B2. Muestra la raíz cuadrada del mismo número.
+#
+# B3. Muestra el arco coseno del mismo número.
 #------------------------------------------------------#
 import math
 
@@ -16,9 +18,18 @@ def raiz(numero):
     return math.sqrt(numero)
 
 
+def arcoCoseno(numero):
+    return math.acos(numero)
+
+
 # B1
 numero = int(input("Dime un número: "))
-print(multPi(numero))
+print(f"{numero} * PI = {multPi(numero)}")
 
 # B2
-print(raiz(numero))
+if(numero >= 0):
+    print(f"Raíz de {numero} = {raiz(numero)}")
+
+# B3
+if numero >= -1 and numero <= 1:
+    print(f"Arco Coseno de {numero}: {arcoCoseno(numero)}")
