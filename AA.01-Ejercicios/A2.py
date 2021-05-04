a1e73b1dafbe4fd947de718bc742bf31b4a2bcb1
David Salazar <davids1_4@hotmail.com>
2021-05-04 10:43:06 +0200

    Añadido ejercicios/A2

--- AA.01-Ejercicios/A2.py
+++ AA.01-Ejercicios/A2.py
@@ -4,15 +4,22 @@
 # cadena = "Hola mundo !!"
 # numero = 100
 # lista = ["T", "R", "W", "A", "G", "M", "Y", "F",
-# "P", "D", #"X",  # "B", "N", "J", "Z", "S", "Q", 
+# "P", "D", #"X",  # "B", "N", "J", "Z", "S", "Q",
 # "V", "H", "L", # # "C", "K", "E"]
 #-----------------------------------------------#
 
 # Pregunta el nombre del operador y muestra un saludo
 # incluyendo el contenido de la variable cadena
-
+# Muestra el saludo, en mayúsculas, minúsculas y con la letra de cada palabra en mayúsculas
 cadena = "Hola mundo !!"
 
-nombre = input("Dime tu nombre: ")
 
-print(f"{cadena}, ¿qué tal {nombre}?")
+def saludo(nombre):
+    return f"{cadena}, ¿qué tal {nombre}?"
+
+
+nombre = input("Dime tu nombre: ")
+print(saludo(nombre))
+print(saludo(nombre).upper())
+print(saludo(nombre).lower())
+print(saludo(nombre).title())
