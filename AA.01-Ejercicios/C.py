127cb3d4b26f292dbb7a60c0e400b77074df59a4
David Salazar <davids1_4@hotmail.com>
2021-05-04 11:45:21 +0200

    Añadido ejercicios/C

---
+++ AA.01-Ejercicios/C.py
@@ -0,0 +1,14 @@
+#-----------------------------------------------------#
+# C1. Muestra el número de elementos de lista.
+# C2. Muestra el primer y el último elemento de lista.
+#-----------------------------------------------------#
+
+lista = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X",
+         "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"]
+
+# C1
+print(len(lista))
+
+# C2
+print(lista[0]) #primer elemento
+print(lista[len(lista)-1]) #último elemento
