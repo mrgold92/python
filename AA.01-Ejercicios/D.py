381780d6f975d1e66a2ac519ebe6c8f33119ee6b
David Salazar <davids1_4@hotmail.com>
2021-05-05 02:13:26 +0200

    Añadido ejercicios/D

--- AA.01-Ejercicios/D.py
+++ AA.01-Ejercicios/D.py
@@ -1,8 +1,16 @@
+# -----------------------------------------------------------------#
 # D1. Pregunta al operador su DNI sin letra.
 # D2. Calcula el resto de dividir el número del DNI entre 23.
-# D3. Muestra el DNI con la letra. El resto de la división representa la posición de la letra del DNI en lista.
+# D3. Muestra el DNI con la letra. El resto de la división
+# representa la posición de la letra del DNI en lista.
+#------------------------------------------------------------------#
 
-def calcularLetraDNI(numeroDNI):
-  return numeroDNI % 23
+lista = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X',
+         'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E']
 
-print(calcularLetraDNI(int(input("Dime tu DNI sin letra."))))
+dni = input("Dime tu DNI sin letra:")
+
+if dni.isdigit():
+    print(f"Tu DNI es: {dni}{lista[int(dni) % 23]}")
+else:
+    print("Debes introducir solo números.")
