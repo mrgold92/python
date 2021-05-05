6cc1166e65ba5e216f3ab2789c68c6b92f070dab
David Salazar <davids1_4@hotmail.com>
2021-05-05 13:50:51 +0200

    Modificado bbddd

--- modulo06/bbdd.py
+++ modulo06/bbdd.py
@@ -5,10 +5,12 @@
 #---------------------------------------------#
 from pymongo import MongoClient
 from pprint import pprint
-
+from bson.objectid import ObjectId
+import json
 
 client = MongoClient('localhost', 27017)
 client = MongoClient("mongodb://localhost:27017/")  # conexión
+
 
 # Seleccionar bbdd
 # conexion.bbdd
@@ -58,6 +60,13 @@ while(result.alive):
 
 result.close()
 
+# Buscar con ascendente 1 o descente -1
+result = client.Northwind.customers.find(
+    {"Country": "USA", "City": "Portland"}).sort("City", -1)
+
+for c in result:
+    pprint(c)
+
 # Saber cuántos documentos hay con un filtro
 cpais = collection.count_documents({'Country': 'USA'})
 print(f"Documentos encontrados: {cpais}")
@@ -84,8 +93,16 @@ multiples2 = collection.find(
 #     pprint(c)
 multiples2.close()
 
+# Buscar por _id
+id = ObjectId("609110ee9fb937350c96c12b")
+result = collection.find_one({"_id": id}).sort('City')
 
-# Añadir documentos
+result = collection.find_one(
+    {"_id": ObjectId("609110ee9fb937350c96c12b")}).sort('City')
+
+
+# Añadir documentos a la colección
+# utilizando un objeto json
 customer = {
     "CustomerID": "DEMO1",
     "CompanyName": "Uno Comidas y Bebidas, SL",
@@ -103,17 +120,86 @@ customer = {
 #idNuevoDoc = collection.insert_one(customer).inserted_id
 #print("Id nuevo doc", idNuevoDoc)
 
-# Editar
-query = {'CustomerID': 'DEMO1'}
+
+# Insertamos un documento utilizando
+# un objeto
+
+class Customer:
+    _id = None
+    CustomerID = None
+    CompanyName = None
+    ContactName = None
+    ContactTitle = None
+    Address = None
+    City = None
+    Region = None
+    PostalCode = None
+    Country = None
+    Phone = None
+    Fax = None
+
+
+cliente = Customer()
+cliente.CustomerID = "Demo 4"
+cliente.CompanyName = "Uno dos Tres, S.L."
+cliente.ContactName = "David Salazar"
+cliente.ContactTitle = "Gerente"
+cliente.Address = "Calle Gran vía 72"
+cliente.City = "Madrid"
+cliente.Region = "Madrid"
+cliente.PostalCode = "28009"
+cliente.Country = "Spain"
+cliente.Phone = "(+34) 91 234 56 78"
+cliente.Fax = "(+34) 91 234 56 78"
+
+# Serializa objeto a JSON
+pprint(json.dumps(cliente.__dict__))
+# Otra forma de serializar a JSON
+pprint(json.dumps(cliente, default=lambda x: x.__dict__))
+
+# idNuevoDoc = client.Northwind.customers.insert_one(json.dumps(cliente,default=lambda x:x.__dict__)).inserted_id
+# print("Id nuevo doc", idNuevoDoc)
+
+idNuevoDoc = client.Northwind.customers.insert_one(
+    cliente.__dict__).inserted_id
+print("Id nuevo doc", idNuevoDoc)
+
+# Editar documentos en una colección
+query = {'Country': 'España'}
 newValues = {
     "$set":
     {
-        "CompanyName": "Restaurante 1, SL",
-        "Address": "Calle Serrano,81",
-        "Phone": "(+34) 91 234 34 59"
+        "Country": "Spain"
     }
 }
 
+# modificar el primero que encuentre
 resultado = collection.update_one(query, newValues)
+# modificar todos los que encuentre
+resultado = collection.update_many(query, newValues)
 print(resultado.matched_count, ' elementos encontrados.')
 print(resultado.modified_count, ' elementos modificados.')
+
+# agregación
+data = client.Northwind.customers.aggregate(
+    [
+        {"$match": {"CustomerID": "ANATR"}},
+        {"$sort": {"City": 1}},
+        {"$lookup": {
+            "from": "orders",
+            "localField": "CustomerID",
+            "foreignField": "CustomerID",
+            "as": "Orders"
+        }}
+    ]
+)
+
+datos = data.next()
+print("ID: ", datos['CustomerID'])
+print("Empresa: ", datos['CompanyName'])
+print("País: ", datos['Country'])
+
+for o in datos['Orders']:
+    print("ID Pedido: ", o['OrderID'])
+    print("Empleado: ", o['EmployeeID'])
+
