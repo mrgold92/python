7318373aecc15a6e292bf1921feedb366e416dcb
David Salazar <davids1_4@hotmail.com>
2021-05-05 04:20:04 +0200

    Actualizados ejercicios/A

--- AA.01-Ejercicios/A.py
+++ AA.01-Ejercicios/A.py
@@ -6,12 +6,17 @@
 # lista = ["T", "R", "W", "A", "G", "M", "Y", "F",
 # "P", "D", #"X",  # "B", "N", "J", "Z", "S", "Q",
 # "V", "H", "L", # # "C", "K", "E"]
+#
+# A1.Pregunta el nombre del operador y muestra un saludo
+# incluyendo el contenido de la variable cadena
+#
+# A2.Muestra el saludo, en mayúsculas, minúsculas y con la letra de cada palabra en mayúsculas
 #-----------------------------------------------#
+
+cadena = "Hola mundo !!"
 
 # Pregunta el nombre del operador y muestra un saludo
 # incluyendo el contenido de la variable cadena
-# Muestra el saludo, en mayúsculas, minúsculas y con la letra de cada palabra en mayúsculas
-cadena = "Hola mundo !!"
 
 
 def saludo(nombre):
@@ -19,7 +24,10 @@ def saludo(nombre):
 
 
 nombre = input("Dime tu nombre: ")
+
 print(saludo(nombre))
+
+# Muestra el saludo, en mayúsculas, minúsculas y con la letra de cada # palabra en mayúsculas
 print(saludo(nombre).upper())
 print(saludo(nombre).lower())
 print(saludo(nombre).title())
