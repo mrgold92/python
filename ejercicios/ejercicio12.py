3b184d40f6ba7a46a642fc3ad4037189f26ccd77
David Salazar <davids1_4@hotmail.com>
2021-05-06 11:10:39 +0200

    Añadido ejercicio 12

---
+++ ejercicios/ejercicio12.py
@@ -0,0 +1,53 @@
+from pymongo import MongoClient
+from pprint import pprint
+import json
+
+# Conexión con MongoDB
+connect = MongoClient("mongodb://localhost:27017/")
+
+# Selección de la BBDD
+db = connect.Northwind
+
+# Selección de colecciones
+products = db.products
+
+print("===================================")
+# Buscar cuántos productos tenemos
+numProductos = products.estimated_document_count()
+print("Número de productos: ", numProductos)
+
+# Buscar y mostrar todos los productos
+for product in products.find():
+    print(f"{product['ProductName']}")
+
+print("===================================")
+# Buscar todos los productos cuyo UnitsInStock sea 0
+# con filter
+listaProductos = products.find()
+pro = list(filter(lambda p: p['UnitsInStock'] == '0', listaProductos))
+
+for p in pro:
+    print(p['ProductName'])
+
+print("===================================")
+# Buscar todos los productos cuyo UnitsInStock sea 0
+# directamente en la bbdd
+lista = products.find({'UnitsInStock': '0'})
+for p in lista:
+    print(p['ProductName'])
+
+print("===================================")
+
+# Coste o valor de nuestro stock
+# De cada producto necesitamos:
+#  - UnitsInStock
+#  - UnitPrice
+
+total = 0
+for p in products.find():
+  total+= int(p['UnitsInStock']) * float(p['UnitPrice'])
+
+print(f"Total en stock: {total:1.2f}€")
+
+print("===================================")
+
