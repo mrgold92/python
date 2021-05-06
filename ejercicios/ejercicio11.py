fd9f44fb70a699c4900d28083edbf720e06e315e
David Salazar <davids1_4@hotmail.com>
2021-05-06 09:54:29 +0200

    Añadido formato en el ejercicio 11

--- ejercicios/ejercicio11.py
+++ ejercicios/ejercicio11.py
@@ -17,9 +17,9 @@ idPedido = input("Identificador del pedido: ")
 pedido = orders.find_one({"OrderID": idPedido})
 
 if(pedido != None):
-    print("================================")
-    print(f" Datos del pedido {idPedido}")
-    print("================================")
+    print("===============================================================")
+    print(f"Datos del pedido {idPedido}")
+    print("===============================================================")
     print(f"Entregar: {pedido['ShipName']}")
     print(f"          {pedido['ShipAddress']}")
     print(f"          {pedido['ShipCity']} ({pedido['ShipCountry']})")
@@ -30,18 +30,23 @@ if(pedido != None):
     # Buscamos y mostramos la descripcióon del producto
     # Descripción - cantidad - precio * cantidad
     detalle = order_details.find({"OrderID": idPedido})
+    print("===============================================================")
+    print(f"{'Nombre':<35} {'cantidad':>6} {'precio':>8} {'total':>8}")
+    print("===============================================================")
+
+    totalPedido = 0
     while(detalle.alive):
         p = detalle.next()
         productId = p['ProductID']
         producto = products.find_one({"ProductID": productId})
-        print(f"Detalles del pedido: {idPedido}")
 
-        total = format(float(p['UnitPrice']) * int(p['Quantity']), '1.2f')
-        print("================================")
-        print(f"{'Nombre':<35} {'cantidad':>6} {'precio':>8} {'total':>8}")
-        print("================================")
-        print(f"{producto['ProductName']:<35}{p['Quantity']:>6} {p['UnitPrice']:>8} {total:>8}")
+        t = float(p['UnitPrice']) * int(p['Quantity'])
+        totalPedido += t
 
+        print(
+            f"{producto['ProductName']:<35} {p['Quantity']:>6} {p['UnitPrice']:>8} {format(t,'1.2f'):>10}")
+    print("===============================================================")
+    print(f"{'TOTAL':>51} {format(totalPedido,'1.2f'):>10}")
 
 else:
     print("No hay resultados.")
