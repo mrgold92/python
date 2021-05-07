7e7d475a99f3fd2b1868bd2cfbac0e7434cb5a6e
David Salazar <davids1_4@hotmail.com>
2021-05-07 14:27:16 +0200

    Actualizado bbdd_SQLServer

--- modulo06/bbdd_SQLServer.py
+++ modulo06/bbdd_SQLServer.py
@@ -11,17 +11,27 @@ cursor = con.cursor(as_dict=True)
 
 #cursor.execute("INSERT INTO Customers (CustomerID, CompanyName,City, Country) VALUES('DEMO3', 'Empresa sin nombre dos', 'Madrid', 'Spain')")
 
-query = "INSERT INTO Customers (CustomerID, CompanyName,City,Country) VALUES(%d, %d, %d, %d)"
+# query = "INSERT INTO Customers (CustomerID, CompanyName,City,Country) VALUES(%d, %d, %d, %d)"
 
-lista = []
-lista.append(('DEXX1', 'EMPRESA 1, SL', 'Madrid', 'Spain'))
-lista.append(('DEXX2', 'EMPRESA 2, SL', 'Madrid', 'Spain'))
-lista.append(('DEXX3', 'EMPRESA 3, SL', 'Madrid', 'Spain'))
-lista.append(('DEXX4', 'EMPRESA 4, SL', 'Madrid', 'Spain'))
+# lista = []
+# lista.append(('DEXX1', 'EMPRESA 1, SL', 'Madrid', 'Spain'))
+# lista.append(('DEXX2', 'EMPRESA 2, SL', 'Madrid', 'Spain'))
+# lista.append(('DEXX3', 'EMPRESA 3, SL', 'Madrid', 'Spain'))
+# lista.append(('DEXX4', 'EMPRESA 4, SL', 'Madrid', 'Spain'))
 
 
-cursor.executemany(query, lista)
+# cursor.executemany(query, lista)
+# con.commit()
+
+
+# cursor.execute("UPDATE Customers SET Country = 'Spain' WHERE Country= 'España'")
+# con.commit()
+# print("Filas afectadas: ",cursor.rowcount)
+
+
+cursor.execute("DELETE FROM Customers WHERE CustomerID LIKE 'DEMO%'")
 con.commit()
+print("Filas afectadas: ",cursor.rowcount)
 
 sys.exit()
 
