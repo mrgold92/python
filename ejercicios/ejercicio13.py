f7219a130c67465f37e469a8ae72cc9ffc78116c
David Salazar <davids1_4@hotmail.com>
2021-05-07 13:40:43 +0200

    Añadido ejercicio13

---
+++ ejercicios/ejercicio13.py
@@ -0,0 +1,42 @@
+import pymssql
+
+
+con = pymssql.connect(server='localhost', user='david',
+                      password='root', database='northwind')
+
+cursor = con.cursor(as_dict=True)
+
+idPedido = input("Identificador del pedido: ")
+
+cursor.execute("SELECT * FROM orders WHERE OrderID = %d", idPedido)
+pedido = cursor.fetchone()
+
+print("===============================================================")
+print(f"Datos del pedido {idPedido}")
+print("===============================================================")
+print(f"Entregar: {pedido['ShipName']}")
+print(f"          {pedido['ShipAddress']}")
+print(f"          {pedido['ShipCity']} ({pedido['ShipCountry']})")
+
+print("===============================================================")
+print(f"{'Nombre':<35} {'cantidad':>6} {'precio':>8} {'total':>8}")
+print("===============================================================")
+
+cursor.execute("SELECT *, UnitPrice * Quantity AS total FROM [Order Details] WHERE OrderID = %d", idPedido)
+
+detalle = cursor.fetchall()
+
+totalPedido = 0
+if(len(detalle) > 0):
+    for p in detalle:
+        cursor.execute("SELECT ProductName FROM products WHERE ProductID = %d", p['ProductID'])
+        producto = cursor.fetchone()
+        totalPedido += p['total']
+        print(f"{producto['ProductName']:<35} {p['Quantity']:>6} {p['UnitPrice']:>8} {format(p['total'],'1.2f'):>10}")
+
+    print("===============================================================")
+    print(f"{'TOTAL':>51} {format(totalPedido,'1.2f'):>10}")
+    cursor.close()
+
+else:
+    print("No hay resultados.")
---
+++ modulo06/bbdd_SQLServer.py
@@ -0,0 +1,35 @@
+import pymssql
+import sys
+
+# con = pymssql.connect(server='LAPTOP-AR7UGD3M',
+#                       database='prueba')
+
+con = pymssql.connect(server='localhost', user='david',
+                      password='root', database='northwind')
+
+cursor = con.cursor(as_dict=True)
+
+#cursor.execute("INSERT INTO Customers (CustomerID, CompanyName,City, Country) VALUES('DEMO3', 'Empresa sin nombre dos', 'Madrid', 'Spain')")
+
+query = "INSERT INTO Customers (CustomerID, CompanyName,City,Country) VALUES(%d, %d, %d, %d)"
+
+lista = []
+lista.append(('DEXX1', 'EMPRESA 1, SL', 'Madrid', 'Spain'))
+lista.append(('DEXX2', 'EMPRESA 2, SL', 'Madrid', 'Spain'))
+lista.append(('DEXX3', 'EMPRESA 3, SL', 'Madrid', 'Spain'))
+lista.append(('DEXX4', 'EMPRESA 4, SL', 'Madrid', 'Spain'))
+
+
+cursor.executemany(query, lista)
+con.commit()
+
+sys.exit()
+
+
+cursor.execute('SELECT * FROM customers')
+
+for row in cursor.fetchall():
+    print(f"{row['City']}")
+
+cursor.close()
+con.close()
