34a06c486d0cba0d8d4601518a4f549b9317ada5
David Salazar <davids1_4@hotmail.com>
2021-05-10 10:41:16 +0200

    Añadido sqlite

---
+++ modulo06/bbdd_SQLite3.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+
+# Si no existe el fichero, lo crea
+con = sqlite3.connect("modulo06\\demo.db")
+
+c = con.cursor()
+
+
+# # command = "SELECT count(name) FROM sqlite_master WHERE type = 'table' and name='alumnos'"
+# # c.execute(command)
+# # tablas = c.fetchone()[0]
+# # if tablas == 0:
+
+# # Siempre necesitamos crear las tablas o comprobar que ya no existan
+# c.execute("""CREATE TABLE IF NOT EXISTS Alumno (id, nombre, apellidos, curso, notas)""")
+
+# # Insercción
+# c.execute("INSERT INTO Alumno VALUES('A00', 'David', 'Salazar', '1A', '')")
+
+# listaAlumnos = [('AR0', 'Ana', 'Trujillo', '1A', ''),
+#                 ('Z02', 'Jimena', 'Sanz', '2A', '')]
+
+# c.executemany('INSERT INTO Alumno VALUES(?, ?, ?, ?, ?)', listaAlumnos)
+
+# con.commit()
+
+
+# Selección
+c.execute("SELECT * FROM Alumno")
+
+for a in c.fetchall():
+  print(a)
+
+con.close()
