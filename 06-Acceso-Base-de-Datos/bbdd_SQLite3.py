b607748da46e7c0e3a5462dfa65e3fa9188a9fb6
David Salazar <davids1_4@hotmail.com>
2021-05-11 07:23:58 +0200

    Actualizado bbdd_SQLite3

--- 06-Acceso-Base-de-Datos/bbdd_SQLite3.py
+++ 06-Acceso-Base-de-Datos/bbdd_SQLite3.py
@@ -7,29 +7,33 @@ con = sqlite3.connect("06-Acceso-Base-de-Datos\\demo.db")
 c = con.cursor()
 
 
-# # command = "SELECT count(name) FROM sqlite_master WHERE type = 'table' and name='alumnos'"
-# # c.execute(command)
-# # tablas = c.fetchone()[0]
-# # if tablas == 0:
+# command = "SELECT count(name) FROM sqlite_master WHERE type = 'table' and name='alumnos'"
+# c.execute(command)
+# tablas = c.fetchone()[0]
+# if tablas == 0:
 
-# # Siempre necesitamos crear las tablas o comprobar que ya no existan
-# c.execute("""CREATE TABLE IF NOT EXISTS Alumno (id, nombre, apellidos, curso, notas)""")
+# Siempre necesitamos crear las tablas en el caso de que no existan
+c.execute(
+    """CREATE TABLE IF NOT EXISTS Alumno (id, nombre, apellidos, curso, notas)""")
 
-# # Insercción
-# c.execute("INSERT INTO Alumno VALUES('A00', 'David', 'Salazar', '1A', '')")
+# Insercción
+c.execute("INSERT INTO Alumno VALUES('A00', 'David', 'Salazar', '1A', '')")
+con.commit()
 
-# listaAlumnos = [('AR0', 'Ana', 'Trujillo', '1A', ''),
-#                 ('Z02', 'Jimena', 'Sanz', '2A', '')]
+# Insercción múltiple
+listaAlumnos = [('AR0', 'Ana', 'Trujillo', '1A', ''),
+                ('Z02', 'Jimena', 'Sanz', '2A', '')]
 
-# c.executemany('INSERT INTO Alumno VALUES(?, ?, ?, ?, ?)', listaAlumnos)
-
-# con.commit()
+c.executemany('INSERT INTO Alumno VALUES(?, ?, ?, ?, ?)', listaAlumnos)
+con.commit()
 
 
 # Selección
 c.execute("SELECT * FROM Alumno")
 
 for a in c.fetchall():
-  print(a)
+    print(a)
 
+# Cerrar conexión y cursor
+c.close()
 con.close()
